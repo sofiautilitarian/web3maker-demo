9a24cf6b4bfd88b318889f5b0bab9c4a9af4739a
Sofia Noor Rafa <sofiautilitarian@email.com>
2025-08-18 16:19:30 +0600

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TopDrawer from './components/TopDrawer.vue';
+
+</script>
+
+<template>
+  <NuxtLayout>
+    <v-app>
+    <TopDrawer />
+    </v-app>
+  </NuxtLayout>
+</template>
